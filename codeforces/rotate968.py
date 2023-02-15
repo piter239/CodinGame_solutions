1b4c431c766e2ef1dbff9cc64d31205591a8adea
piter239 <piter239@gmail.com>
2023-02-15 20:45:14 +0100

    no-recursion approach, still wrong answer at test 5

--- codeforces/rotate968.py
+++ codeforces/rotate968.py
@@ -15,106 +15,58 @@ def rotate(s, begin, end):
     return s[:begin] + r + s[end:]
 
 
-def find_rotations(s, f, begin, end):
+def find_rotations(s, f):
     """ for strings s, f
-    return a list of pairs within [begin:end]
-    such that rotations of substrings from s for each pair results in f (considered from begin to end)
+    return a list of pairs
+    such that rotations of substrings from s for each pair results in f
     pairs are considered 0-indexed positions in s
-    empty list means no possible rotations found.
-    Always: end > begin"""
-    assert end > begin
+    empty list means no possible rotations found."""
+
     if len(s) != len(f):
         return []
-    if s[begin:end].count('8') != f[begin:end].count('8'):
+    if s.count('8') != f.count('8'):
         return []
+    begin = 0
+    end = len(s)
 
-    if s[begin:end] == f[begin:end]:
-        return [[begin, end], [begin, end]]
+    # no-recursion approach: bring all 8's onto the same position in both strings,
+    # then just rotate the remaining unequal substrings of the length 1
+    res = []
+    i = j = begin
+    while i < end and s[i:end].count('8'):
+        i = s[i:end].index('8') + i
+        j = f[j:end].index('8') + j
+        assert j < end
+        if i != j:
+            a, b = min(i, j), max(i, j)
+            f = rotate(f, a, b + 1)
+            res += [[a, b]]
+        i = j = min(i, j) + 1
 
-    if rotate(s[begin:end], 0, end - begin) == f[begin:end]:
-        return [[begin, end]]
+    for i, c in enumerate(s):
+        if s[i] != f[i]:
+            res += [[i, i]]
+            f = rotate(f, i, i + 1)
 
-    matching_substrings = []
-    i = begin
-    while i < end:
-        j = i
-        while s[j] == f[j] and j < end:
-            j += 1
-            if j == end:
-                break
-        if j > i:
-            matching_substrings.append((i, j))
-            i = j
-        else:
-            i += 1
-    # print(matching_substrings)
-
-    if not matching_substrings:
-        return []
-
-    for a, b in matching_substrings:
-        if begin == a:
-            r2 = [find_rotations(s, f, b, end)]
-            if all(r2):
-                return r2[0]
-            r3 = [find_rotations(s, f, a, end)]
-            if all(r3):
-                return r3[0]
-
-        elif end == b:
-            r1 = [find_rotations(s, f, begin, a)]
-            if all(r1):
-                return r1[0]
-            r2 = [find_rotations(s, f, begin, b)]
-            if all(r2):
-                return r2[0]
-
-        else:
-            r1 = [find_rotations(s, f, begin, a), find_rotations(s, f, b, end)]
-            if all(r1):
-                return r1[0] + r1[1]
-            r2 = [find_rotations(s, f, begin, b), find_rotations(s, f, b, end)]
-            if all(r2):
-                return r2[0] + r2[1]
-            r3 = [find_rotations(s, f, begin, a), find_rotations(s, f, a, end)]
-            if all(r3):
-                return r3[0] + r3[1]
-    return []
+    assert s == f
+    return res
 
 
-# print(rotate(s, 0, len(s)-1))
+result = find_rotations(start, finish)
 
-res = find_rotations(start, finish, 0, len(start))
-
-if res:
-    print(len(res))
-    for p in res:
-        print(p[0] + 1, p[1])
+if result:
+    print(len(result))
+    for p in result:
+        print(p[0] + 1, p[1] + 1)
 else:
     print(-1)
 
 """
-# 98 699 99
-# 98 66 669
-# 99 86 669
-# 699 98 66
-
 test1
 9869999
 6999866
 
-9869999
-9866669
-
-9866669
-9986669
-
-9986669
-6999866
-
-# 9 99
-  9669899
-# 6999866
-
-
+test2
+988686
+968988
 """
